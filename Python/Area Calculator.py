8b5edc42616f7316832947946dc9a68132337018
Mark Cantimbuhan <cantimbuhanml@students.nu-dasma.edu.ph>
2025-03-12 20:31:44 +0800

    Create Area Calculator.py

---
+++ Python/Area Calculator.py
@@ -0,0 +1,61 @@
+from math import pi
+def Menu():
+    print("""
+==================
+Area Calculator 📐
+==================
+
+1) Triangle
+2) Rectangle
+3) Square
+4) Circle
+5) Quit""")
+
+def triangle():
+    print()
+    Height = int(input("Height: "))
+    Base = int(input("Base: "))
+
+    area = (Height*Base)/2
+    print(f"\nThe area is {area:.2f}")
+
+def rectangle():
+    print()
+    Length = int(input("Length: "))
+    Width = int(input("Width: "))
+
+    area = Length * Width
+    print(f"\nThe area is {area:.2f}")
+
+def square():
+    print()
+    Side = int(input("Side: "))
+
+    area = Side**2
+    print(f"\nThe area is {area:.2f}")
+
+def circle():
+    print()
+    Radius = int(input("Radius: "))
+
+    area = pi*(Radius**2)
+    print(f"\nThe area is {area:.2f}")
+
+def main():
+    while True:
+        Menu()
+        shape = input("\nWhich shape: ")
+        match(shape.lower()):
+            case "triangle":
+                triangle()
+            case "rectangle":
+                rectangle()
+            case "square":
+                square()
+            case "circle":
+                circle()
+            case "quit":
+                print("Exiting...")
+                break
+
+main()
